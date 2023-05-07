75e9ebc4ed3c4238440f19bc79e5389f3b91be54
d3mah-24 <ahmedshitech2011@gmail.com>
2023-05-07 13:24:18 +0300

    Add: Multi Language

--- Bot.py
+++ Bot.py
@@ -8,6 +8,8 @@ import telebot
 from telebot import types
 from telebot.types import ReplyKeyboardRemove
 
+from LANG import *
+
 bot = telebot.TeleBot('6034077201:AAHICg_ZMoB6knQ8atPjdmmt9DgvKE_Obec')
 
 data = {}
@@ -23,15 +25,17 @@ def start_command(message):
         if len(data[my_id]) > 2:
             if data[my_id]["phone_number"]:
                 name = data[my_id]["real_name"]
-                bot.send_message(message.chat.id, f"Welcome back, {name}! ",
+                bot.send_message(message.chat.id, Welcome_back[data[my_id]["lang"]].format(name),
                                  reply_markup=menu())
+
             else:
                 keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-                button = types.KeyboardButton(text="Send Phone Number", request_contact=True)
+                button = types.KeyboardButton(text=Send_Phone[data[my_id]["lang"]], request_contact=True)
+
                 keyboard.add(button)
-                bot.send_message(message.chat.id, f"Please click 'Send Phone Number' button", reply_markup=keyboard)
+                bot.send_message(message.chat.id, Please_Send_Phone[data[my_id]["lang"]], reply_markup=keyboard)
         else:
-            bot.send_message(message.chat.id, "Hi! Please enter your name.", reply_markup=ReplyKeyboardRemove())
+            bot.send_message(message.chat.id, enter_your_name[data[my_id]["lang"]], reply_markup=ReplyKeyboardRemove())
 
     elif message.text.startswith('/start '):
         with open('coupon.json', 'r') as fl:
@@ -39,38 +43,39 @@ def start_command(message):
         coupon_code = message.text.split(' ')[1]
         user_id = coupon[coupon_code]["id"]
         if coupon_code in coupon and my_id not in coupon[coupon_code]["referred"]:
-            data[my_id] = {"invited": user_id}
+            data[my_id] = {"invited": user_id, "lang": "ENGLISH"}
             coupon[coupon_code]["referred"].append(my_id)
             coupon[coupon_code]["count"] += 1
             with open("coupon.json", "w") as fg:
                 json.dump(dict(coupon), fg, indent=4)
         with open("users.json", "w") as fg:
             json.dump(dict(data), fg, indent=4)
-        bot.send_message(message.chat.id, "Hi! Please enter your name.", reply_markup=ReplyKeyboardRemove())
+        bot.send_message(message.chat.id, enter_your_name[data[my_id]["lang"]], reply_markup=ReplyKeyboardRemove())
 
         return
     else:
         data[my_id] = {"invited": None}
         with open("users.json", "w") as fg:
             json.dump(dict(data), fg, indent=4)
-        bot.send_message(message.chat.id, "Hi! Please enter your name.", reply_markup=ReplyKeyboardRemove())
+        bot.send_message(message.chat.id, enter_your_name[data[my_id]["lang"]], reply_markup=ReplyKeyboardRemove())
 
 
 @bot.message_handler(func=lambda message: message.text == "Delivery")
 def Delivery(message):
     with open('users.json', 'r') as f:
         data = json.load(f)
-        if str(message.from_user.id) in data:
-            if data[str(message.from_user.id)]["phone_number"]:
-                bot.send_message(message.chat.id, "Ertib from Tulu dimtu", reply_markup=delivery_choice())
+        my_id = str(message.from_user.id)
+        if my_id in data:
+            if data[my_id]["phone_number"]:
+                bot.send_message(message.chat.id, ertib_menu[data[my_id]["lang"]], reply_markup=delivery_choice())
             else:
                 keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-                button = types.KeyboardButton(text="Send Phone Number", request_contact=True)
+                button = types.KeyboardButton(text=Send_Phone[data[my_id]["lang"]], request_contact=True)
                 keyboard.add(button)
-                bot.send_message(message.chat.id, f"Please click 'Send Phone Number' button", reply_markup=keyboard)
+                bot.send_message(message.chat.id, Please_Send_Phone[data[my_id]["lang"]], reply_markup=keyboard)
                 return
         else:
-            bot.send_message(message.chat.id, "Please register first /start.")
+            bot.send_message(message.chat.id, register_first[data[my_id]["lang"]])
 
 
 @bot.message_handler(func=lambda message: message.text == "Stationary")
@@ -83,16 +88,16 @@ def Referral(message):
     user_id = str(message.from_user.id)
     with open('users.json', 'r') as f:
         data = json.load(f)
-        if str(user_id) in data:
+        if user_id in data:
             with open("coupon.json", "r") as file:
                 coupon = json.load(file)
             link = data[user_id]["link"]
             count = coupon[data[user_id]["coupon_code"]]["count"]
             coins = coupon[data[user_id]["coupon_code"]]["coins"]
-            bot.send_message(message.chat.id, f"Your Referral link :- {link} \n")
+            bot.send_message(message.chat.id, referral_link[data[user_id]["lang"]].format(link))
             bot.send_message(message.chat.id,
-                             f"You Invited {count} Persons\n"
-                             f"You Have {coins} Coin{['s', ''][coins > 1]}."
+                             invite_have[data[user_id]["lang"]].format(count, coins)
+
                              )
 
 
@@ -105,56 +110,54 @@ def Help(message):
 def Pprint(message):
     with open('users.json', 'r') as f:
         data = json.load(f)
-        if str(message.from_user.id) in data:
-            if data[str(message.from_user.id)]["phone_number"]:
-                bot.send_message(message.chat.id, "Please send your file in doc or pdf format")
+        my_id = str(message.from_user.id)
+        if my_id in data:
+            if data[my_id]["phone_number"]:
+                bot.send_message(message.chat.id, send_file[data[my_id]["lang"]])
 
             else:
                 keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-                button = types.KeyboardButton(text="Send Phone Number", request_contact=True)
+                button = types.KeyboardButton(text=send_file[data[my_id]["lang"]], request_contact=True)
                 keyboard.add(button)
-                bot.send_message(message.chat.id, f"Please click 'Send Phone Number' button", reply_markup=keyboard)
+                bot.send_message(message.chat.id, Please_Send_Phone[data[my_id]["lang"]], reply_markup=keyboard)
                 return
         else:
-            bot.send_message(message.chat.id, "Please register first /start.")
+            bot.send_message(message.chat.id, register_first[data[my_id]["lang"]])
 
 
 @bot.message_handler(content_types=['audio', 'photo', 'voice', 'video', 'document', 'sticker'])
 def handle_document(message):
-    # filter only PDF and DOCX files
-    # print(message.document)
+    my_id = str(message.from_user.id)
+    with open('users.json', 'r') as f:
+        data = json.load(f)
     try:
         if message.document.file_name.endswith('.docx') or message.document.file_name.endswith(
                 '.doc') or message.document.file_name.endswith('.pdf'):
-            with open('users.json', 'r') as f:
-                data = json.load(f)
-                if str(message.from_user.id) in data:
-                    if data[str(message.from_user.id)]["phone_number"]:
+            if my_id in data:
+                if data[my_id]["phone_number"]:
+                    file_id = message.document.file_id
+                    file_info = bot.get_file(file_id)
+                    file_path = file_info.file_path
+                    file_name = message.document.file_name
+                    # print(file_name, 999)
+                    downloaded_file = bot.download_file(file_path)
+                    with open(file_name, 'wb') as new_file:
+                        new_file.write(downloaded_file)
+                    bot.send_message(message.chat.id, what[data[my_id]["lang"]], reply_markup=print_choice(file_name))
 
-                        # save the file to disk
-                        file_id = message.document.file_id
-                        file_info = bot.get_file(file_id)
-                        file_path = file_info.file_path
-                        file_name = message.document.file_name
-                        # print(file_name, 999)
-                        downloaded_file = bot.download_file(file_path)
-                        with open(file_name, 'wb') as new_file:
-                            new_file.write(downloaded_file)
-                        bot.send_message(message.chat.id, "What do you want", reply_markup=print_choice(file_name))
-
-                    else:
-                        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-                        button = types.KeyboardButton(text="Send Phone Number", request_contact=True)
-                        keyboard.add(button)
-                        bot.send_message(message.chat.id, f"Please click 'Send Phone Number' button",
-                                         reply_markup=keyboard)
                 else:
-                    bot.send_message(message.chat.id, "Please register first /start.")
+                    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+                    button = types.KeyboardButton(text=Send_Phone[data[my_id]["lang"]], request_contact=True)
+                    keyboard.add(button)
+                    bot.send_message(message.chat.id, Please_Send_Phone[data[my_id]["lang"]],
+                                     reply_markup=keyboard)
+            else:
+                bot.send_message(message.chat.id, register_first[data[my_id]["lang"]])
         else:
-            bot.send_message(message.chat.id, "Sorry, We only accept PDF or DOC and DOCX files.")
+            bot.send_message(message.chat.id, sorry_file[data[my_id]["lang"]])
             bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
     except:
-        bot.send_message(message.chat.id, "Sorry, We only accept PDF or DOC and DOCX files.")
+        bot.send_message(message.chat.id, sorry_file[data[my_id]["lang"]])
         bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
 
 
@@ -208,14 +211,10 @@ def callback_handler(call):
                     price = num_pages * print_price_list[typ.split('+')[0]]
                     markup = types.InlineKeyboardMarkup()
                     btn = types.InlineKeyboardButton(
-                        "Click this button after you paid",
+                        after_paid[data[str(user_id)]["lang"]],
                         callback_data=f"--{num_pages}+{file_name}+{typ.split('+')[0]}")
                     markup.add(btn)
-                    cap = f"Total Pages :- {num_pages}\n" \
-                          f"Price :- {price}\n" \
-                          f"Payment Methods\n" \
-                          f"    - CBE (100987543)\n" \
-                          f"    - Telebirr (0987654321)"
+                    cap = Welcome_back[data[str(user_id)]["lang"]].format(num_pages, price)
                     bot.send_message(call.message.chat.id, cap, reply_markup=markup)
                     bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
                 elif "--" in call.data:
@@ -239,9 +238,7 @@ def callback_handler(call):
 
                     bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
 
-                    bot.send_message(user_id, "Thank you for working with us.\n"
-                                              "Within 5 Min we will call you if you paid.\n"
-                                              "if don't get any response from us contact us 0912345678 ")
+                    bot.send_message(user_id, thank_you[data[str(user_id)]["lang"]])
                 elif "==" in call.data:
                     query = call.data[2:]
                     markup = types.InlineKeyboardMarkup(row_width=3)
@@ -252,7 +249,7 @@ def callback_handler(call):
                     markup.add(*a[6:9])
                     markup.add(*a[9:12])
                     bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
-                    bot.send_message(user_id, "How much do you want", reply_markup=markup)
+                    bot.send_message(user_id, how_much[data[str(user_id)]["lang"]], reply_markup=markup)
                 elif "((" in call.data:
                     if "Real" in typ:
                         with open('coupon.json', 'r') as file:
@@ -266,24 +263,19 @@ def callback_handler(call):
                                 json.dump(dict(coupon), files, indent=4)
                         bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id,
                                                       reply_markup=None)
-                        bot.send_message(idd, "Payment verified")
+                        bot.send_message(idd, payment_verified[data[str(user_id)]["lang"]])
 
                         return
-                    bot.send_message(typ.split("_")[-1], "Payment not verified")
+                    bot.send_message(typ.split("_")[-1], payment_not_verified[data[str(user_id)]["lang"]])
                     bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
                 elif "||" in call.data:
                     no, typpe = call.data[2:].split("_")
                     price = int(no) * ertib_price_list[typpe]
                     markup = types.InlineKeyboardMarkup()
                     btn = types.InlineKeyboardButton(
-                        "Click this button after you paid", callback_data=f"!!{no}+{price}+{typpe}")
+                        after_paid[data[str(user_id)]["lang"]], callback_data=f"!!{no}+{price}+{typpe}")
                     markup.add(btn)
-                    cap = f"Item     :- {typpe} Ertib\n" \
-                          f"Quantity :- {no}\n" \
-                          f"Price    :- {price}\n" \
-                          f"Payment Methods  \n" \
-                          f"   - CBE (100987543) \n" \
-                          f"   - Telebirr (0987654321) "
+                    cap = payment_not_verified[data[str(user_id)]["lang"]].format(typpe, no, price)
                     bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
                     bot.send_message(call.message.chat.id, cap, reply_markup=markup)
                 elif "!!" in call.data:
@@ -305,18 +297,16 @@ def callback_handler(call):
                     bot.send_message(chat_id="-1001674209692", text=cap, reply_markup=mark)
 
                     bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
-                    bot.send_message(user_id, "Thank you for working with us.\n"
-                                              "Within 5 Min we will call you if you paid.\n"
-                                              "if don't get any response from us contact us 0912345678 ")
+                    bot.send_message(user_id, thank_you[data[str(user_id)]["lang"]])
             else:
                 keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-                button = types.KeyboardButton(text="Send Phone Number", request_contact=True)
+                button = types.KeyboardButton(text=Send_Phone[data[str(user_id)]["lang"]], request_contact=True)
                 keyboard.add(button)
-                bot.send_message(call.message.chat.id, f"Please click 'Send Phone Number' button",
+                bot.send_message(call.message.chat.id, Please_Send_Phone[data[str(user_id)]["lang"]],
                                  reply_markup=keyboard)
 
         else:
-            bot.send_message(user_id, "Please register first /start.")
+            bot.send_message(user_id, register_first[data[str(user_id)]["lang"]])
 
 
 @bot.message_handler(func=lambda message: True)
@@ -326,27 +316,27 @@ def name_input(message):
     with open('users.json', 'r') as f:
         data = json.load(f)
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button = types.KeyboardButton(text="Send Phone Number", request_contact=True)
+    button = types.KeyboardButton(text=Send_Phone[data[str(user_id)]["lang"]], request_contact=True)
     keyboard.add(button)
 
     if user_id in data:
         if len(data[user_id]) < 2:
             if "09" in text or "251" in text:
                 bot.send_message(message.chat.id,
-                                 f"Please, Don't write your phone number just click 'Send Phone Number' button",
+                                 dont_write[data[user_id]["lang"]],
                                  reply_markup=keyboard)
                 return
             if " " not in text or len(text) < 7:
-                bot.send_message(message.chat.id, "Please, enter your full name.", reply_markup=ReplyKeyboardRemove())
+                bot.send_message(message.chat.id,  full_name[data[user_id]["lang"]], reply_markup=ReplyKeyboardRemove())
                 return
             with open('users.json', 'w') as fz:
                 data[str(message.from_user.id)]["real_name"] = text
                 data[str(message.from_user.id)]["phone_number"] = None
                 json.dump(dict(data), fz, indent=4)
-            bot.send_message(message.chat.id, f"Thanks {text}! Please click 'Send Phone Number' button.",
+            bot.send_message(message.chat.id, thanks_click[data[user_id]["lang"]].format(text),
                              reply_markup=keyboard)
         elif data[user_id]["phone_number"] is None:
-            bot.send_message(message.chat.id, f"Please click 'Send Phone Number' button.",
+            bot.send_message(message.chat.id,Please_Send_Phone[data[user_id]["lang"]],
                              reply_markup=keyboard)
 
     else:
@@ -354,7 +344,7 @@ def name_input(message):
             data[str(message.from_user.id)]["real_name"] = text
             data[str(message.from_user.id)]["phone_number"] = None
             json.dump(dict(data), fz, indent=4)
-        bot.send_message(message.chat.id, f"Thanks {text}! Please click 'Send Phone Number' button.",
+        bot.send_message(message.chat.id,  thanks_click[data[user_id]["lang"]].format(text),
                          reply_markup=keyboard)
 
 
@@ -403,9 +393,9 @@ def phone_input(message):
 
                 json.dump(dict(data), files, indent=4)
             bot.send_message(
-                user_id, "Thanks for registering,\n Choose an option:", reply_markup=menu())
+                user_id, thanks_register[data[user_id]["lang"]], reply_markup=menu())
         else:
-            bot.send_message(user_id, "Please register first /start.")
+            bot.send_message(user_id,register_first[data[user_id]["lang"]])
 
 
 while True:
---
+++ LANG.py
@@ -0,0 +1,147 @@
+Welcome_back = {
+    "ENGLISH": "Welcome back, {}! ",
+    "AMHARIC": "ሰላም {},\n /menu",
+    "AFFAN_OROMO": "Akkam {},\n /menu",
+    "ትግርኛ": "ሰላም {},\n /menu"
+}
+
+Send_Phone = {
+    "ENGLISH": "Send Phone Number",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+
+Please_Send_Phone = {
+    "ENGLISH": "Please click 'Send Phone Number' button",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+
+enter_your_name = {
+    "ENGLISH": "Hi! Please enter your name.",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+ertib_menu = {
+    "ENGLISH": "Ertib from Tulu dimtu",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+register_first = {
+    "ENGLISH": "Please register first /start.",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+
+referral_link = {
+    "ENGLISH": "Your Referral link :- {} \n",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+invite_have = {
+    "ENGLISH": "You Invited {} Persons\nYou Have {} Coin.",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+
+send_file = {
+    "ENGLISH": "Please send your file in doc or pdf format",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+
+what = {
+    "ENGLISH": "What do you want",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+sorry_file = {
+    "ENGLISH": "Sorry, We only accept PDF or DOC and DOCX files.",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+
+after_paid = {
+    "ENGLISH": "Click this button after you paid",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+
+payment = {
+    "ENGLISH": "Total Pages :- { }\nPrice :- { }\nPayment Methods\n    - CBE (100987543)\n    - Telebirr (0987654321)",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+
+thank_you = {
+    "ENGLISH": "Thank you for working with us.\nWithin 5 Min we will call you if you paid.\nif don't get any response "
+               "from us contact us 0912345678 ",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+how_much = {
+    "ENGLISH": "How much do you want",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+payment_verified = {
+    "ENGLISH": "Payment verified",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+payment_not_verified = {
+    "ENGLISH": "Payment not verified",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+payment_not_verified = {
+    "ENGLISH": "Item     :- { } Ertib\n" \
+               "Quantity :- { }\n" \
+               "Price    :- { }\n" \
+               "Payment Methods  \n" \
+               "   - CBE (100987543) \n" \
+               "   - Telebirr (0987654321) ",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+dont_write = {
+    "ENGLISH": "Please, Don't write your phone number just click 'Send Phone Number' button",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+full_name = {
+    "ENGLISH":  "Please, enter your full name.",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+thanks_click = {
+    "ENGLISH":  "Thanks {}! Please click 'Send Phone Number' button.",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
+thanks_register = {
+    "ENGLISH":   "Thanks for registering,\n Choose an option:",
+    "AMHARIC": "",
+    "AFFAN_OROMO": "",
+    "ትግርኛ": ""
+}
